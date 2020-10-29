e8e86db8eb8ff5b18faaaa1c7d8e409d5187b4e9
Abenx <5195693+rushairer@users.noreply.github.com>
2020-10-29 12:15:04 +0800

    [Fixed]Fix bug #2.
    Closes #2.

--- Sources/ABWheelPickerModifier/ABWheelPickerModifier.swift
+++ Sources/ABWheelPickerModifier/ABWheelPickerModifier.swift
@@ -12,18 +12,17 @@ import SwiftUI
 @available(watchOS 6.0, *)
 
 public class ABWheelPickerModifierData: ObservableObject {
-    @Published public fileprivate(set) var displayAngle: Int = 0
+    @Published public fileprivate(set) var displayAngle: CGFloat = 0
     @Published public var minimumAngle: Int = 0
     @Published public var maximumAngle: Int = 360
 
     @Published fileprivate(set) var realAngle: CGFloat = 0
     @Published fileprivate(set) var lastAngle: CGFloat = 0
-    @Published fileprivate(set) var virtualAngle: CGFloat = 0
-    @Published fileprivate(set) var roundedNumber: CGFloat = 0
+    @Published fileprivate(set) var lastPoint: CGPoint = CGPoint.zero
     
-    private var initAngle: Int = 0
+    private var initAngle: CGFloat = 0
     
-    public init(initAngle:Int = 0) {
+    public init(initAngle:CGFloat = 0) {
         self.initAngle = initAngle
         self.reset()
     }
@@ -31,10 +30,8 @@ public class ABWheelPickerModifierData: ObservableObject {
     public func reset() {
         self.lastAngle = CGFloat(self.initAngle)
         self.realAngle = self.lastAngle
-        self.virtualAngle = self.realAngle
         self.displayAngle = self.initAngle
-        
-        self.roundedNumber = (self.realAngle - self.realAngle.truncatingRemainder(dividingBy: 360.0)) / 360.0
+        self.lastPoint = CGPoint.zero
     }
 }
 
@@ -43,8 +40,18 @@ public class ABWheelPickerModifierData: ObservableObject {
 @available(watchOS 6.0, *)
 
 public struct ABWheelPickerModifier: ViewModifier {
+    
+    enum Direction {
+        case none
+        case clockwise
+        case counterclockwise
+    }
+    
     @ObservedObject var data: ABWheelPickerModifierData
     
+    @State private var direction: Direction = .none
+    @State private var lastDirection: Direction = .none
+
     public var longPressGestureOnChanged: ((LongPressGesture.Value) -> Void)?
     public var longPressGestureOnEnded: ((LongPressGesture.Value) -> Void)?
     public var dragGestureOnChanged: ((DragGesture.Value) -> Void)?
@@ -79,70 +86,35 @@ public struct ABWheelPickerModifier: ViewModifier {
             
             let dragGesture = DragGesture()
                 .onChanged{ value in
+                    let theta = (atan2(value.location.x - geometry.size.width / 2, geometry.size.width / 2 - value.location.y) - atan2(value.startLocation.x - geometry.size.width / 2, geometry.size.width / 2 - value.startLocation.y)) * 180 / .pi
                     
-                    var theta = (atan2(value.location.x - geometry.size.width / 2, geometry.size.width / 2 - value.location.y) - atan2(value.startLocation.x - geometry.size.width / 2, geometry.size.width / 2 - value.startLocation.y)) * 180 / .pi
-                    
-                    let predictedTheta = (atan2(value.predictedEndLocation.x - geometry.size.width / 2, geometry.size.width / 2 - value.predictedEndLocation.y) - atan2(value.location.x - geometry.size.width / 2, geometry.size.width / 2 - value.location.y)) * 180 / .pi
-                    
-                    if theta < 0 { theta += 360  }
-                    
-                    let fixedTheta = (self.data.lastAngle + theta).truncatingRemainder(dividingBy: 360.0)
-                    let mod = self.data.realAngle.truncatingRemainder(dividingBy: 360.0)
-                    
-                    //var roundedNumberVariation: CGFloat = 0
-                    
-                    if predictedTheta > 0 && predictedTheta < 100 ||  predictedTheta < -300{
-                        if mod >= 300 && mod <= 360 && fixedTheta >= 0 && fixedTheta <= 60 {
-                            self.data.roundedNumber += 1
-                            //roundedNumberVariation = 1
-                        }
-                    } else if predictedTheta < 0 && predictedTheta > -100 || predictedTheta > 300 {
-                        if fixedTheta >= 300 && fixedTheta <= 360 && mod >= 0 && mod <= 60 {
-                            self.data.roundedNumber -= 1
-                            //roundedNumberVariation = -1
-                        }
+                    if self.data.lastPoint == CGPoint.zero {
+                        self.data.lastPoint = value.startLocation
                     }
                     
+                    var offsetTheta = (atan2(value.location.x - geometry.size.width / 2, geometry.size.width / 2 - value.location.y) - atan2(self.data.lastPoint.x - geometry.size.width / 2, geometry.size.width / 2 - self.data.lastPoint.y)) * 180 / .pi
+                    
+                    if offsetTheta < -90 {
+                        offsetTheta += 360
+                    }
+                    
+                    if offsetTheta > 90 {
+                        offsetTheta -= 360
+                    }
+                    
+                    self.data.displayAngle += offsetTheta
                     self.data.realAngle = round(theta + self.data.lastAngle)
-                    self.data.displayAngle = Int(round(fixedTheta + 360.0 * self.data.roundedNumber))
-                    /*
-                    //Check
-                    let newAngle = Int(round(fixedTheta + 360.0 * (self.data.roundedNumber + roundedNumberVariation)))
                     
-                    /*
-                    guard newAngle <= self.data.maximumAngle else {
-                        self.data.realAngle = CGFloat(self.data.maximumAngle)
-                        return
-                    }
-                    guard newAngle >= self.data.minimumAngle else {
-                        self.data.realAngle = CGFloat(self.data.minimumAngle)
-                        return
-                    }
-                    */
-                    print(newAngle)
-
-                    self.data.virtualAngle = round(theta + self.data.lastAngle)
-                    self.data.roundedNumber += roundedNumberVariation
-
-                    self.data.realAngle = self.data.virtualAngle
-                    self.data.displayAngle = newAngle
+                    self.data.lastPoint = value.location
                     
-                    if newAngle <= self.data.maximumAngle && newAngle >= self.data.minimumAngle {
-                        
-                    } else {
-                        return
-                    }
-                    */
                     if self.dragGestureOnChanged != nil {
                         self.dragGestureOnChanged!(value)
                     }
                 }
                 .onEnded { value in
                     self.data.lastAngle = self.data.realAngle
-                    /*
-                    self.data.virtualAngle = self.data.realAngle
-                    self.data.roundedNumber = (self.data.realAngle - self.data.realAngle.truncatingRemainder(dividingBy: 360.0)) / 360.0
-                    */
+                    self.data.lastPoint = CGPoint.zero
+                    
                     if self.dragGestureOnEnded != nil {
                         self.dragGestureOnEnded!(value)
                     }
--- Tests/ABWheelPickerModifierTests/ABWheelPickerModifierTests.swift
+++ Tests/ABWheelPickerModifierTests/ABWheelPickerModifierTests.swift
@@ -6,7 +6,7 @@ final class ABWheelPickerModifierTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ABWheelPickerModifier().text, "Hello, World!")
+        //XCTAssertEqual(ABWheelPickerModifier().text, "Hello, World!")
     }
 
     static var allTests = [
