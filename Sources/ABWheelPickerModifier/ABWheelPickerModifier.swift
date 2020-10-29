5e9b4278d3da83fb83745b266f10685b59e8f375
Abenx <5195693+rushairer@users.noreply.github.com>
2020-10-29 18:02:15 +0800

    [Added]Add minimumAngle, maximumAngle. (#5)
    Close #5.

--- Sources/ABWheelPickerModifier/ABWheelPickerModifier.swift
+++ Sources/ABWheelPickerModifier/ABWheelPickerModifier.swift
@@ -12,26 +12,34 @@ import SwiftUI
 @available(watchOS 6.0, *)
 
 public class ABWheelPickerModifierData: ObservableObject {
-    @Published public fileprivate(set) var displayAngle: CGFloat = 0
-    @Published public var minimumAngle: Int = 0
-    @Published public var maximumAngle: Int = 360
+    @Published public fileprivate(set) var value: Int = 0
+    @Published public var minimumValue: Int = 0
+    @Published public var maximumValue: Int = 360
 
     @Published fileprivate(set) var realAngle: CGFloat = 0
     @Published fileprivate(set) var lastAngle: CGFloat = 0
     @Published fileprivate(set) var lastPoint: CGPoint = CGPoint.zero
     
-    private var initAngle: CGFloat = 0
+    private var initValue: Int = 0
     
-    public init(initAngle:CGFloat = 0) {
-        self.initAngle = initAngle
+    public init(initValue:Int = 0) {
+        self.initValue = initValue
         self.reset()
     }
     
     public func reset() {
-        self.lastAngle = CGFloat(self.initAngle)
-        self.realAngle = self.lastAngle
-        self.displayAngle = self.initAngle
+        self.lastAngle = CGFloat(self.initValue)
+        withAnimation {
+            self.realAngle = self.lastAngle
+        }
+        self.value = self.initValue
         self.lastPoint = CGPoint.zero
+    }
+    
+    public func setRange(_ minimumValue: Int, _ maximumValue: Int) -> ABWheelPickerModifierData {
+        self.minimumValue = minimumValue
+        self.maximumValue = maximumValue
+        return self
     }
 }
 
@@ -52,40 +60,34 @@ public struct ABWheelPickerModifier: ViewModifier {
     @State private var direction: Direction = .none
     @State private var lastDirection: Direction = .none
 
-    public var longPressGestureOnChanged: ((LongPressGesture.Value) -> Void)?
-    public var longPressGestureOnEnded: ((LongPressGesture.Value) -> Void)?
+    @State private var pauseDragging = false
+    
     public var dragGestureOnChanged: ((DragGesture.Value) -> Void)?
     public var dragGestureOnEnded: ((DragGesture.Value) -> Void)?
-    
+    public var minimumValueOnChanged: ((DragGesture.Value) -> Void)?
+    public var maximumValueOnChanged: ((DragGesture.Value) -> Void)?
+
     public init(_ data: ABWheelPickerModifierData,
-                longPressGestureOnChanged: ((LongPressGesture.Value) -> Void)?,
-                longPressGestureOnEnded: ((LongPressGesture.Value) -> Void)?,
-                dragGestureOnChanged: ((DragGesture.Value) -> Void)?,
-                dragGestureOnEnded: ((DragGesture.Value) -> Void)?
+                dragGestureOnChanged: ((DragGesture.Value) -> Void)? = nil,
+                dragGestureOnEnded: ((DragGesture.Value) -> Void)? = nil,
+                minimumValueOnChanged: ((DragGesture.Value) -> Void)? = nil,
+                maximumValueOnChanged: ((DragGesture.Value) -> Void)? = nil
     ) {
         self.data = data
-        self.longPressGestureOnChanged = longPressGestureOnChanged
-        self.longPressGestureOnEnded = longPressGestureOnEnded
         self.dragGestureOnChanged = dragGestureOnChanged
         self.dragGestureOnEnded = dragGestureOnEnded
+        self.minimumValueOnChanged = minimumValueOnChanged
+        self.maximumValueOnChanged = maximumValueOnChanged
     }
     
     public func body(content: Content) -> some View {
         func internalView(geometry: GeometryProxy) -> some View {
-            let pressGesture = LongPressGesture()
-                .onChanged({ value in
-                    if self.longPressGestureOnChanged != nil {
-                        self.longPressGestureOnChanged!(value)
-                    }
-                })
-                .onEnded { value in
-                    if self.longPressGestureOnEnded != nil {
-                        self.longPressGestureOnEnded!(value)
-                    }
-                }
-            
-            let dragGesture = DragGesture()
+            let dragGesture = DragGesture(minimumDistance: 0, coordinateSpace: .local)
                 .onChanged{ value in
+                    assert(self.data.value >= self.data.minimumValue && self.data.value <= self.data.maximumValue, "The value must be between minimumValue and maximumValue.")
+                    
+                    guard !self.pauseDragging else { return }
+
                     let theta = (atan2(value.location.x - geometry.size.width / 2, geometry.size.width / 2 - value.location.y) - atan2(value.startLocation.x - geometry.size.width / 2, geometry.size.width / 2 - value.startLocation.y)) * 180 / .pi
                     
                     if self.data.lastPoint == CGPoint.zero {
@@ -102,16 +104,35 @@ public struct ABWheelPickerModifier: ViewModifier {
                         offsetTheta -= 360
                     }
                     
-                    self.data.displayAngle += offsetTheta
-                    self.data.realAngle = round(theta + self.data.lastAngle)
+                    let virtualAngle = CGFloat(self.data.value) + offsetTheta
                     
-                    self.data.lastPoint = value.location
-                    
-                    if self.dragGestureOnChanged != nil {
-                        self.dragGestureOnChanged!(value)
+                    if virtualAngle > CGFloat(self.data.maximumValue) {
+                        if self.maximumValueOnChanged != nil {
+                            self.maximumValueOnChanged!(value)
+                        }
+                        self.data.value = self.data.maximumValue
+                        self.data.realAngle = CGFloat(self.data.value)
+                        self.pauseDragging = true
+                    } else if virtualAngle < CGFloat(self.data.minimumValue) {
+                        if self.minimumValueOnChanged != nil {
+                            self.minimumValueOnChanged!(value)
+                        }
+                        self.data.value = self.data.minimumValue
+                        self.data.realAngle = CGFloat(self.data.value)
+                        self.pauseDragging = true
+                    } else {
+                        self.data.value += Int(offsetTheta)
+                        self.data.realAngle = round(theta + self.data.lastAngle)
+                        self.data.lastPoint = value.location
+                        
+                        if self.dragGestureOnChanged != nil {
+                            self.dragGestureOnChanged!(value)
+                        }
                     }
                 }
                 .onEnded { value in
+                    self.pauseDragging = false
+                    
                     self.data.lastAngle = self.data.realAngle
                     self.data.lastPoint = CGPoint.zero
                     
@@ -119,12 +140,10 @@ public struct ABWheelPickerModifier: ViewModifier {
                         self.dragGestureOnEnded!(value)
                     }
                 }
-            
-            let combined = pressGesture.simultaneously(with: dragGesture)
-            
+                        
             return content
                 .rotationEffect(.degrees(Double(self.data.realAngle)))
-                .gesture(combined)
+                .gesture(dragGesture)
         }
         
         return GeometryReader(content: internalView(geometry:))
